cf1c9f99cd87f9028023ccd311dcfa9edd9a03aa
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-09-05 12:40:42 +0300

    update app.css for a deep milky transfromation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,15 @@
-import './App.css';
-
+import  {BrowserRouter , Routes, Route} from "react-router-dom";
+import Home from "./Home";
+import Explore from "./Explore";
 
 export default function App () {
 return (
-<div className="app-container">
-<h1>WELCOME TO  TEAM EXPLORE </h1>
-
-
-<p> Team explore is a platform that helps you to explore the Galaxy,visit each planet learnt about the galaxy,the mon the planet and the stars</p>
-
-
-<button>Lets explore the Galaxy</button>
-
-</div>
+<BrowserRouter>
+<Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/explore" element={<Explore />} />  
+</Routes>
+</BrowserRouter>
 
 );
-
 }
-
