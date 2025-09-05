61ec82c6b1242d0d7ce04927ee0b0a923efa80ef
MuthoniGathiithi <gathiithijoyce74@gmail.com>
2025-09-05 11:37:53 +0300

    Modified App.jsx and App.css to use Galaxy

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ return (
 <h1>WELCOME TO  TEAM EXPLORE </h1>
 
 
-<p> Team explore is a platform that helps you to explore the world,visit each planet learnt about the galaxy,the mon the planet and the stars</p>
+<p> Team explore is a platform that helps you to explore the Galaxy,visit each planet learnt about the galaxy,the mon the planet and the stars</p>
 
 
-<button>Lets explore the world</button>
+<button>Lets explore the Galaxy</button>
 
 </div>
 
